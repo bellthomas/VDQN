2b83a8a8692effd05933ad1175907a7644dd081c
Harri Bell-Thomas <harribt@live.co.uk>
2020-01-06 21:49:37 +0000

    Added base model and posterior functions

--- VDQN.py
+++ VDQN.py
@@ -13,6 +13,8 @@ class VDQN:
             self.__stateSpace = stateSpace
             self.__actionSpace = actionSpace
             self.__hiddenLayers = hiddenLayers
+            self.__nnLayers = [stateSpace] + hiddenLayers + [actionSpace]
+
             self.__session = session
             self.__optimiser = optimiser
 
@@ -32,14 +34,33 @@ class VDQN:
 
 
         def __model(self):
-            pass
+            with. tf.variable_scope("prior", reuse=True):
+                stateX = tf.placeholder(tf.float32, [None, self.__stateSpace], name="stateX")
+                actionX = tf.placeholder(tf.int32, [None], name="actionX")
+                selected_actionX = tf.one_hot(actionX, self.__actionSpace, dtype=tf.float32)
+                
+                activation = tf.nn.relu(tf.matmul(stateX, self.__W[0]) + self.__b[0])
+                layers = len(self.__W.keys())
+                for i in range(1, layers-1):
+                    activation = tf.nn.relu(tf.matmul(activation, self.__W[i]) + self.__b[i])
+                activation = tf.matmul(activation, self.__W[layers-1]) + self.__b[layers-1]
+                
+                chosenAction = tf.reduce_sum(
+                    tf.multiply(activation, selected_actionX),
+                    axis=1
+                )
+                chosenActionDistribution = edm.Normal(loc=chosenAction, scale=self.__sigma, name="Y")
+
+                self.__stateX = stateX
+                self.__actionX = actionX
+                self.__nextAction = chosenActionDistribution
 
         def __forwardComputation(self):
             pass
 
         def __prior(self, W_sigma, b_sigma):
             with tf.variable_scope("prior"):
-                nnLayers = [self.__stateSpace] + self.__hiddenLayers + [self.__actionSpace]
+                nnLayers = self.__nnLayers
                 _numTransitions = len(nnLayers) - 1
                 _defaultTransitionWeights = _numTransitions * [0.1]
                 W_sigma = W_sigma or _defaultTransitionWeights[:]
@@ -69,7 +90,49 @@ class VDQN:
                 self.__shape_b = _shape_b
 
         def __posterior(self):
-            pass
+            with tf.variable_scope("posterior"):
+                nnLayers = self.__nnLayers
+                layerTransitionPairings = zip(nnLayers[:-1], nnLayers[1:])
+                _index = 0
+                sigmaRho = self.__sigmaRho or np.log(np.exp(0.017)-1.0)
+
+                # Posterior collections.
+                self.__posterior_W, self.__posterior_b = {}, {}
+                self.__posterior_W_mu, self.__posterior_b_mu = {}, {}
+                self.__posterior_W_rho, self.__posterior_b_rho = {}, {}
+
+                for _left, _right in layerTransitionPairings:
+                    with tf.variable_scope("posterior_W{}".format(_index)):
+                        _width = np.sqrt(3 / _left)
+                        self.__posterior_W_mu[_index] = tf.Variable(
+                            tf.random_uniform([_left, _right], -1*_width, _width),
+                            name="mean"
+                        )
+                        self.__posterior_W_rho[_index] = tf.Variable(
+                            tf.random_uniform([_left, _right], sigmaRho, sigmaRho),
+                            name="std", trainable=True
+                        )
+                        self.__posterior_W[_index] = edm.Normal(
+                            loc = self.__posterior_W_mu[_index],
+                            scale = tf.nn.softplus(self.__posterior_W_rho[_index])
+                        )
+
+                    with tf.variable_scope("posterior_b{}".format(_index)):
+                        self.__posterior_b_mu[_index] = tf.Variable(
+                            tf.random_uniform([_right], 0, 0),
+                            name="mean"
+                        )
+                        self.__posterior_b_rho[_index] = tf.Variable(
+                            tf.random_uniform([_right], sigmaRho, sigmaRho),
+                            name="std", trainable=True
+                        )
+                        self.__posterior_b[_index] = edm.Normal(
+                            loc = self.__posterior_b_mu[_index],
+                            scale = tf.nn.softplus(self.__posterior_b_rho[_index])
+                        )
+
+                    _index += 1
+
 
         def __inference(self):
             pass
