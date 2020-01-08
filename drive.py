0b666470a9cddacaba4a47e2b507ba7554cef5c9
Harri Bell-Thomas <harribt@live.co.uk>
2020-01-08 19:55:36 +0000

    Import random v2

--- drive.py
+++ drive.py
@@ -44,7 +44,7 @@ def process_alive(proc):
 num_processes = 2
 processes = []
 # available_affinities = list(range(cores))
-next_experiment = 0 if not loop else random.ranint(0, len(experiments))
+next_experiment = 0 if not loop else random.randint(0, len(experiments))
 
 while(next_experiment < len(experiments)):
     # Remove completed processes.
