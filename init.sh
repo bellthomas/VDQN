8b979d6b7a9da21b15db18831b595113b762912f
Harri Bell-Thomas <harribt@live.co.uk>
2020-01-08 20:21:33 +0000

    Install tf from url

--- init.sh
+++ init.sh
@@ -5,8 +5,7 @@ sudo apt-get install python3-venv -y
 python3 -m venv env
 source env/bin/activate
 
-curl https://github.com/mind/wheels/releases/download/tf1.4.1-cpu/tensorflow-1.4.1-cp36-cp36m-linux_x86_64.whl -O
-pip3 install tensorflow-1.4.1-cp36-cp36m-linux_x86_64.wh
+pip3 --no-cache-dir install https://github.com/mind/wheels/releases/download/tf1.4.1-cpu/tensorflow-1.4.1-cp36-cp36m-linux_x86_64.whl
 pip3 install "gym==0.12.1"
 pip3 uninstall numpy -y
 pip3 install "numpy<1.17.0"
