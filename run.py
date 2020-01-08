00f93e4d0bf81b84391f9d118fad0cbda6c69a28
Harri Bell-Thomas <harribt@live.co.uk>
2020-01-08 17:40:01 +0000

    Now writing log lines to log file

--- run.py
+++ run.py
@@ -25,12 +25,19 @@ def execute(algorithm, env, episodes, timesteps, seed=100, lr=1e-2, silent=False
     output_path.mkdir(parents=True, exist_ok=True)
     output_dir_abs = output_path.absolute().as_posix()
 
+    output_file_index = 0
+    output_file = Path("{}/{}".format(output_dir_abs, output_file_index))
+    while output_file.exists():
+        output_file_index += 1
+        output_file = Path("{}/{}".format(output_dir_abs, output_file_index))
+    output_file_uri = output_file.absolute().as_posix()
+
     # Build concifg object
     config = AlgorithmConfig({
         "output_path": output_dir_abs,
         "episodes": episodes,
         "environment": env,
-        "post_episode": lambda x: handlePostEpisode(x, silent, variational=(algorithm in ["VDQN","DVDQN"])),
+        "post_episode": lambda x: handlePostEpisode(x, output_file_uri, silent, variational=(algorithm in ["VDQN","DVDQN"])),
         "maximum_timesteps": timesteps
     })
 
@@ -43,7 +50,7 @@ def execute(algorithm, env, episodes, timesteps, seed=100, lr=1e-2, silent=False
     func = switcher.get(algorithm, lambda: sys.exit("No algorithm: {}".format(algorithm)))
     func()
 
-def handlePostEpisode(data, silent, variational=False):
+def handlePostEpisode(data, output, silent, variational=False):
     dataline = ("Episode {0} (i: {1}, {2} seconds) --- r: {3} (avg: {4}){5}".format(
         data.get("episode", "-1"),
         data.get("iteration", "-1"),
@@ -57,6 +64,8 @@ def handlePostEpisode(data, silent, variational=False):
 
     if not silent:
         print(dataline)
+    with open(output, "a+") as log:
+        log.write("{}\n".format(dataline))
 
 def main():
     argparser = ArgumentParser(description='VDQN/DQN Demonstrator')
