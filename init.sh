3d752c281333a2315d6395b066f206a2fad3fca2
Harri Bell-Thomas <harribt@live.co.uk>
2020-01-08 20:18:01 +0000

    Added init script v2

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ source env/bin/activate
 
 curl https://github.com/mind/wheels/releases/download/tf1.4.1-cpu/tensorflow-1.4.1-cp36-cp36m-linux_x86_64.whl -O
 pip3 install tensorflow-1.4.1-cp36-cp36m-linux_x86_64.wh
-pip3 install "gym=0.12.1"
+pip3 install "gym==0.12.1"
 pip3 uninstall numpy -y
 pip3 install "numpy<1.17.0"
 pip3 install "chainer<7.0.0"
