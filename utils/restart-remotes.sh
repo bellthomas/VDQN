cfc92ef080dd543c984395e9a81410e22657a4dc
Harri Bell-Thomas <harribt@live.co.uk>
2020-01-09 17:05:18 +0000

    New restart remote script v2

---
+++ utils/restart-remotes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+while read h; do
+    ADDR="$h"
+    ssh -i ~/Desktop/hc-Default.pem ubuntu@$ADDR "cd ~/VDQN; git pull; ./restart-remote.sh"
+    echo $ADDR
+done <hosts.txt
