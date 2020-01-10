400cd1696c0ffe45423f9fdb00d205b5550b536d
Harri Bell-Thomas <harribt@live.co.uk>
2020-01-10 18:25:08 +0000

    Force bin-bash in restart-remote script v2

--- utils/restart-remote.sh
+++ utils/restart-remote.sh
@@ -3,4 +3,5 @@ cd ~/VDQN
 tmux kill-server
 git pull origin master
 tmux new-session -d -s evaluation
-tmux send -t evaluation "/bin/bash; source env/bin/activate; python3 drive.py" ENTER
+tmux send -t evaluation "/bin/bash" ENTER
+tmux send -t evaluation "source env/bin/activate; /bin/bash python3 drive.py" ENTER
