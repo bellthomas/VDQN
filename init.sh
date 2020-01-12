5733ba42018978e02c45136f934bfbf0beda0935
Harri Bell-Thomas <harribt@live.co.uk>
2020-01-12 01:24:31 +0000

    Added gym atari install to init

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ source env/bin/activate
 
 pip3 --no-cache-dir install https://github.com/mind/wheels/releases/download/tf1.4.1-cpu/tensorflow-1.4.1-cp36-cp36m-linux_x86_64.whl
 pip3 install "gym==0.12.1"
+pip3 install "gym[atari]"
 pip3 uninstall numpy -y
 pip3 install "numpy<1.17.0"
 pip3 install "chainer<7.0.0"
